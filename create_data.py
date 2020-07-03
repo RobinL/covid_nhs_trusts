ebd59ceb85455c7c96b1dccde0b36dc9dbf208bb
Robin Linacre <robinlinacre@hotmail.com>
2020-07-03 13:24:57 +0100

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -22,7 +22,7 @@ for link in a:
         
 filename = href.split("/")[-1].replace(".xlsx", "").replace(".xls", "").lower()
 
-df = pd.read_excel(href, sheet_name="Tab4 Deaths by trust", skiprows=15, usecols="B:EA")
+df = pd.read_excel(href, sheet_name="Tab4 Deaths by trust", skiprows=15, usecols="B:IA")
 
 
 cols = list(df.columns)
